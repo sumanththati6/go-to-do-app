7a8ef13077cf1d5d76ca38f7880f81393821b029
codingwithkapil <kapil.ks58410@gmail.com>
2023-12-19 17:17:16 +0530

    working on chages

--- go-server/models/models.go
+++ go-server/models/models.go
@@ -4,6 +4,14 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type ToDoList struct {
 	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	User   User               `gorm:"foreignKey:UserID,constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
 	Task   string             `json:"task,omitempty"`
 	Status bool               `json:"status,omitempty"`
+}
+
+type User struct {
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name     string             `json:"name,omitempty"`
+	Email    string             `json:"email,omitempty" gorm:"unique_index"`
+	Password string             `json:"password,omitempty"`
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/go-to-do-app
+
+go 1.21.5
